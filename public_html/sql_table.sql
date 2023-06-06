ec34d17d09c1487c2dc8ff065636763adee937f1
Krayus <suazo.diego@gmail.com>
2023-06-06 13:39:43 -0400

    Delete public_html directory

--- public_html/sql_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE round_robin (
-	team_id INT(10),
-	staff_id INT(10),
-	token BIT
-)
--- public_html/upload.php
+++
@@ -1,8 +0,0 @@
-<?php
-$archivos = $_FILES['files'];
-
-foreach ( $archivos['tmp_name'] as $indice => $tmp_name){
-    $nombre_real = $archivos['name'][$indice];
-    move_upload_file($tmp_name, "uploads/$nombre_real" );
-}
-//echo json_encode($archivos);
